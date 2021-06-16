d4fd0860420618889fe82c9b08fa5cd24be85c17
wajahataz <54248569+wajahatabbas27@users.noreply.github.com>
2021-06-17 04:00:50 +0500

    added graphql function and updated netlify.toml

--- functions/grapql/grapql.js
+++ functions/grapql/grapql.js
@@ -1,24 +1,30 @@
-const { ApolloServer, gql } = require('apollo-server-lambda')
+const { ApolloServer, gql } = require('apollo-server-lambda');
 
+// Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
     hello: String
-    }
-`
+  }
+`;
 
-
+// Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello, world!'
+    hello: () => 'Hello world!',
   },
-},
-
+};
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-})
 
-const handler = server.createHandler()
+  // By default, the GraphQL Playground interface and GraphQL introspection
+  // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
+  //
+  // If you'd like to have GraphQL Playground and introspection enabled in production,
+  // the `playground` and `introspection` options must be set explicitly to `true`.
+  playground: true,
+  introspection: true,
+});
 
-module.exports = { handler }
+exports.handler = server.createHandler();
